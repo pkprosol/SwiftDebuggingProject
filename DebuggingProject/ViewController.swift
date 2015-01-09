87d2ee3e3258ca179c730418ddd27c1e425fa575
pkprosol <peter.prosol@gmail.com>
2015-01-09 12:41:40 +0100

    spacing

--- DebuggingProject/ViewController.swift
+++ DebuggingProject/ViewController.swift
@@ -50,6 +50,7 @@ class ViewController: UIViewController {
         
         println(result)
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
